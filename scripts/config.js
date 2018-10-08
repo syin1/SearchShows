dc5da7965dbbf5400d883befbb14e37ba1c98da8
syin <shaomingyinut@gmail.com>
2018-10-08 19:19:56 -0400

    clean up folder structure

--- scripts/config.js
+++
@@ -1,4 +0,0 @@
-//MY API KEY.
-const config = {
-    API_KEY : "07be10560c3c4cf68794acc1da83356b"
-}
--- server.js
+++ server.js
@@ -4,8 +4,8 @@
 // ******************************************************************************
 // *** Dependencies
 // =============================================================
-var express = require("express");
-var bodyParser = require("body-parser");
+var express = require('express');
+var bodyParser = require('body-parser');
 
 // Sets up the Express App
 // =============================================================
@@ -13,7 +13,7 @@ var app = express();
 var PORT = process.env.PORT || 8080;
 
 // Requiring our models for syncing
-var db = require("./models");
+var db = require('./models');
 
 // Sets up the Express app to handle data parsing
 
@@ -23,17 +23,17 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // Static directory
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 // Routes
 // =============================================================
-require("./routes/api-routes.js")(app);
-require("./routes/html-routes.js")(app);
+require('./routes/api-routes.js')(app);
+require('./routes/html-routes.js')(app);
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
 db.sequelize.sync({ force: true }).then(function() {
   app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
+    console.log('App listening on PORT ' + PORT);
   });
 });
